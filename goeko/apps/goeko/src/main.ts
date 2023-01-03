fe54afaac8e527eb865ea85ad20cc21dbffe1c6e
Oswald <cuman74@gmail.com>
2023-01-03 18:14:38 +0100

    fix despliegue en pro

--- goeko/apps/goeko/src/main.ts
+++ goeko/apps/goeko/src/main.ts
@@ -1,4 +1,4 @@
-import { loadRemoteModule, setRemoteDefinitions } from '@nrwl/angular/mf';
+import { setRemoteDefinitions } from '@nrwl/angular/mf';
 import { environment } from './environments/environment';
 
 let url_module_federation_manifest = '/assets/module-federation.manifest.json';  
