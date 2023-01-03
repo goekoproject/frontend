fb89583f86db463e9f19652cd807f7138282e460
Oswald <cuman74@gmail.com>
2023-01-03 18:58:23 +0100

    fix

--- goeko/apps/goeko/webpack.prod.config.js
+++ goeko/apps/goeko/webpack.prod.config.js
@@ -2,21 +2,17 @@ const { withModuleFederation } = require('@nrwl/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  output: {
-    publicPath: 'auto',
-    scriptType: 'text/javascript',
-  },
-  remotes: [
-    ['home', 'https://goeko-c8a23.web.app/'],
-    ['app2', 'https://app2.example.com'],
-  ],
+/*   output: {
+		publicPath: 'auto',
+		scriptType: 'text/javascript',
+	}, */
   /*
    * Remote overrides for production.
    * Each entry is a pair of an unique name and the URL where it is deployed.
    *
    * e.g.
    * remotes: [
-   *   ['home':'https://goeko-c8a23.web.app/'],
+   *   ['home':'https://goeko-c8a23.web.app/],
    *   ['app2', 'https://app2.example.com'],
    * ]
    */
