5ae8108597959a15a986ed3704833f7c4b0045f5
Oswald <cuman74@gmail.com>
2023-01-03 22:29:43 +0100

    Revert "fix despliegue en pro"
    This reverts commit ecc616a78837e04ffbddf962859a8fef58a72320.

--- goeko/apps/goeko/src/main.ts
+++ goeko/apps/goeko/src/main.ts
@@ -1,12 +1,6 @@
-import { loadRemoteModule, setRemoteDefinitions } from '@nrwl/angular/mf';
-import { environment } from './environments/environment';
+import { setRemoteDefinitions } from '@nrwl/angular/mf';
 
-let url_module_federation_manifest = '/assets/module-federation.manifest.json';  
-if (environment.production) { 
-  url_module_federation_manifest = '/assets/module-federation.manifest.pro.json';
-}
-
-fetch(url_module_federation_manifest)
+fetch('/assets/module-federation.manifest.json')
   .then((res) => res.json())
   .then((definitions) => setRemoteDefinitions(definitions))
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
