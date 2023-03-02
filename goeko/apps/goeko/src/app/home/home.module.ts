de4766e61b477c4864f9be5d84242ea3bd28942f
Oswald <cuman74@gmail.com>
2023-03-02 21:08:10 +0100

    change landing and new landing

--- goeko/apps/goeko/src/app/home/home.module.ts
+++ goeko/apps/goeko/src/app/home/home.module.ts
@@ -1,25 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { HomeRouteModule } from './home.routes';
-import { BannerComponent } from './banner/banner.component';
-import { ButtonModule } from '@goeko/ui';
+import { NgModule } from '@angular/core';
 import { ContentFulModule } from '@goeko/store';
+import { ButtonModule } from '@goeko/ui';
+import { BannerComponent } from './banner/banner.component';
+import { HomeRouteModule } from './home.routes';
+import { HomeComponent } from './home/home.component';
 
-import { ContentComponent } from './content/content.component';
 import { ModelComponent } from './banner/model/model.component';
-import { SphereComponent } from './banner/sphere/sphere.component';
 import { ScenesComponent } from './banner/scenes/scenes.component';
+import { SphereComponent } from './banner/sphere/sphere.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    BannerComponent,
-    ContentComponent,
-    ModelComponent,
-    SphereComponent,
-    ScenesComponent,
-  ],
-  imports: [CommonModule, HomeRouteModule, ButtonModule, ContentFulModule],
+	declarations: [HomeComponent, BannerComponent, ContentComponent, ModelComponent, SphereComponent, ScenesComponent],
+	imports: [CommonModule, HomeRouteModule, ButtonModule, ContentFulModule],
 })
 export class HomeModule {}
--- goeko/apps/goeko/src/app/home/home.routes.ts
+++ goeko/apps/goeko/src/app/home/home.routes.ts
@@ -1,17 +1,16 @@
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
+	{
+		path: '',
+		component: HomeComponent,
+	},
 ];
 
-
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class HomeRouteModule {}
