f86385d277dc813cce47c718a72c54fe69a221cd
Oswald <cuman74@gmail.com>
2023-12-18 08:49:05 +0100

    fix deploy

--- goeko/apps/goeko/src/app/app.component.ts
+++ goeko/apps/goeko/src/app/app.component.ts
@@ -42,7 +42,6 @@ export class AppComponent implements OnInit, AfterViewInit {
 		private _authService: AuthService
 	) {
 		this._routeChange();
-		console.log(this.showPopupCookies);
 	}
 
 	ngAfterViewInit(): void {
