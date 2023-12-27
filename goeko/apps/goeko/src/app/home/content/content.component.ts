c3e15ef9d46e6ac80b561904024902c614cff6cf
Oswald <cuman74@gmail.com>
2023-12-27 14:39:55 +0100

    feat: new landing page

--- goeko/apps/goeko/src/app/home/content/content.component.ts
+++ goeko/apps/goeko/src/app/home/content/content.component.ts
@@ -6,6 +6,7 @@ import {
   QueryList,
   ViewChildren,
   ViewEncapsulation,
+  effect,
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { map } from 'rxjs';
@@ -13,6 +14,15 @@ import { InteractionService } from '../banner/scenes/interaction.service';
 import { HomeService } from '../home.service';
 import { CONTENT } from './content.contants';
 import { TranslateService } from '@ngx-translate/core';
+
+enum CONTENT_TYPE_DATA {
+  ACTORS = 'actores',
+  LANDING_PAGE = 'landingPage',
+}
+enum ENTRYS_ID {
+  CONNECTING = '5K722xUKUczzRHt5COUgLp',
+  SUSTAINABILITY = '2hwIsU3aiyyd2RhzFIgvw0',
+}
 @Component({
   selector: 'go-content',
   templateUrl: './content.component.html',
@@ -26,8 +36,6 @@ export class ContentComponent implements OnInit {
   otherContent = false;
   badStuffIcons = ['bolt', 'compost', 'unknown_document'];
   actorsImg = ['sme.jpg', 'cleantech.jpg', 'bank.jpg'];
-
-  private _body: any;
 
   public content = CONTENT;
 
@@ -52,29 +60,44 @@ export class ContentComponent implements OnInit {
       .map((benefits: any) => benefits.data.target.fields);
   };
   link: any;
+
+  public actors!: any;
+  public entryDataConnecting!: { text: string };
+  public entryDataSustainability!: { text: string };
+
   constructor(
     private _homeService: HomeService,
     private _router: Router,
     private _translate: TranslateService,
     private _interactionService: InteractionService,
     private _viewportScroller: ViewportScroller
-  ) {}
+  ) {
+    this._effectActors();
+  }
 
   ngOnInit(): void {
     this.currentLang = this._translate.defaultLang;
     this._getContentActors();
+    this._getContentDataActors();
     this._getActors();
-    this._onClickSme();
     this._onClickCleanTeach();
     this._oBank();
+    this._homeService.getSloganSustainability(ENTRYS_ID.SUSTAINABILITY);
+    this._homeService.getSloganConnecting(ENTRYS_ID.CONNECTING);
+
+    console.log(this.actors);
   }
 
-  private _onClickSme() {
-    this._interactionService.onSMEClick.subscribe((res) => {
-      if (res) {
-        this._viewportScroller.scrollToAnchor('sme');
-      }
+  private _effectActors() {
+    effect(() => {
+      this.actors = this._homeService.dataContentTypeSignal();
+      this.entryDataConnecting = this._homeService.entryDataConnecting();
+      this.entryDataSustainability =
+        this._homeService.entryDataSustainability();
     });
+  }
+  navigateToActors(actor: string) {
+    this._viewportScroller.scrollToAnchor(actor);
   }
 
   private _onClickCleanTeach() {
@@ -91,18 +114,25 @@ export class ContentComponent implements OnInit {
 
   private _getActors() {
     this._translate.onLangChange.subscribe((res) => {
+      this._getContentDataActors();
       this._getContentActors();
       this.currentLang = res.lang;
     });
   }
 
+  private _getContentDataActors() {
+    this._homeService
+      .getContentType(CONTENT_TYPE_DATA.ACTORS)
+      .subscribe((res) => {
+        this.articles = res.map((actor: any) => ({
+          ...actor,
+          benefits: this._buildDataLandingPage(actor.benefits),
+          image: actor.image?.fields?.file?.url,
+        }));
+      });
+  }
+
   private _getContentActors() {
-    this._homeService.getContentType('actores').subscribe((res) => {
-      this.articles = res.map((actor: any) => ({
-        ...actor,
-        benefits: this._buildDataLandingPage(actor.benefits),
-        image: actor.image?.fields?.file?.url,
-      }));
-    });
+    this._homeService.getContentTypeSignal(CONTENT_TYPE_DATA.LANDING_PAGE);
   }
 }
--- goeko/apps/goeko/src/app/home/home.service.ts
+++ goeko/apps/goeko/src/app/home/home.service.ts
@@ -1,20 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { ContentFulService, LangOfLocalecontentFul } from '@goeko/store';
 import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class HomeService {
-	constructor(private _contentFul: ContentFulService) {}
+  dataContentTypeSignal = signal<unknown | null>(null);
+  entryDataConnecting = signal<any | null>(null);
+  entryDataSustainability = signal<any | null>(null);
 
-	getContent() {
-		return this._contentFul.getContentEntry();
-	}
+  constructor(private _contentFul: ContentFulService) {}
 
-	getEntry(entryId: string) {
-		return this._contentFul.getEntryId(entryId);
-	}
+  getContent() {
+    return this._contentFul.getContentEntry();
+  }
 
-	getContentType(contentType: string) {
-		return this._contentFul.getContentType(contentType).pipe(map((res) => res.items.map((item) => item.fields)));
-	}
+  getEntry(entryId: string) {
+    return this._contentFul.getEntryId(entryId);
+  }
+
+  getSloganConnecting(entryId: string) {
+    this.getEntry(entryId)
+      .pipe(map((res) => res.fields))
+      .subscribe((entryData: any) => {
+        if (entryData) {
+          this.entryDataConnecting.set(entryData);
+        }
+      });
+  }
+  getSloganSustainability(entryId: string) {
+    this.getEntry(entryId)
+      .pipe(map((res) => res.fields))
+      .subscribe((entryData: any) => {
+        if (entryData) {
+          this.entryDataSustainability.set(entryData);
+        }
+      });
+  }
+  getContentType(contentType: string) {
+    return this._contentFul
+      .getContentType(contentType)
+      .pipe(map((res) => res.items.map((item) => item.fields)));
+  }
+
+  getContentTypeSignal(contentType: string) {
+    return this._contentFul
+      .getContentType(contentType)
+      .pipe(map((res) => res.items.map((item) => item.fields)))
+      .subscribe((dataContentTypeSignal) => {
+        if (dataContentTypeSignal) {
+          this.dataContentTypeSignal.set(dataContentTypeSignal);
+        }
+      });
+  }
 }
