ef410d306f27427bd11cd6d1ba95340d77e37e31
Belén Jiménez <b.jimenezsan5@gmail.com>
2024-06-04 13:11:45 +0200

    New feature admin-page

---
+++ goeko/apps/goeko/src/app/admin/data-admin/data-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'goeko-data-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-admin.component.html',
+  styleUrl: './data-admin.component.css',
+})
+export class DataAdminComponent {}
