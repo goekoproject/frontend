aaae2aaa6d1b345cf6b9fe212e42620d9fd87183
Oswald <cuman74@gmail.com>
2024-06-29 18:29:49 +0200

    refactor: Update propagateDataUser method in user.services.ts

--- goeko/libs/store/src/lib/user/user.services.ts
+++ goeko/libs/store/src/lib/user/user.services.ts
@@ -43,7 +43,7 @@ export class UserService {
       )
       .subscribe((data) => {
         if (data) {
-          this.propagateDataUser(true)
+          this.propagateDataUser(data)
         }
         this.fechAuthUser.next(true)
       })
