d4a7e41ab0601a4f409dcb7d71f4fc45f830db50
Oswald <cuman74@gmail.com>
2024-06-29 17:06:26 +0200

    refactor: Update header component to improve logo size and scrolling behavior

--- goeko/apps/goeko/src/app/home/header/header.component.ts
+++ goeko/apps/goeko/src/app/home/header/header.component.ts
@@ -1,14 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  Renderer2,
-  ViewChild,
-  ViewEncapsulation,
-} from '@angular/core';
-import { AuthService, LANGS } from '@goeko/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild, ViewEncapsulation } from '@angular/core'
+import { AuthService, LANGS } from '@goeko/core'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'goeko-header',
@@ -21,42 +13,39 @@ import { TranslateService } from '@ngx-translate/core';
   },
 })
 export class HeaderComponent implements OnInit {
-  @ViewChild('header', { static: true }) header!: ElementRef;
-  @ViewChild('logo', { static: true }) logo!: ElementRef;
+  @ViewChild('header', { static: true }) header!: ElementRef
+  @ViewChild('logo', { static: true }) logo!: ElementRef
 
   @HostListener('window:scroll', ['$event'])
   onScroll($event: any) {
     if (window.scrollY > 0) {
-      this._renderer.setStyle(this.header.nativeElement, 'maxHeight', '6rem');
-      this._renderer.setStyle(this.logo.nativeElement, 'width', '7%');
+      this._renderer.setStyle(this.header.nativeElement, 'maxHeight', '6rem')
+      this._renderer.setStyle(this.logo.nativeElement, 'width', '7%')
     } else {
-      this._renderer.setStyle(this.header.nativeElement, 'maxHeight', '10rem');
-      this._renderer.setStyle(this.logo.nativeElement, 'width', '12%');
+      this._renderer.setStyle(this.header.nativeElement, 'maxHeight', '10rem')
+      this._renderer.setStyle(this.logo.nativeElement, 'width', '12%')
     }
   }
 
-  langs = LANGS;
-  defaultLang!: any;
+  langs = LANGS
+  defaultLang!: any
 
   constructor(
     private _renderer: Renderer2,
     private translate: TranslateService,
-    private _authService: AuthService
+    private _authService: AuthService,
   ) {}
 
   ngOnInit(): void {
-    this.defaultLang = this.langs.find(
-      (lang) => lang.code === this.translate.getDefaultLang()
-    );
+    this.defaultLang = this.langs.find((lang) => lang.code === this.translate.getDefaultLang())
   }
-  onChangeLangs(selectedLand: any) {
-    this.translate.use(selectedLand.code);
+  onChangeLangs(selectedCodeLand: string) {
+    this.translate.use(selectedCodeLand)
   }
   goTologin() {
     this._authService.universalLogin().subscribe({
-      next:(v) => console.log('response',v),
-      error:(r) => console.log('error',r)
-      }
-    );
+      next: (v) => console.log('response', v),
+      error: (r) => console.log('error', r),
+    })
   }
 }
