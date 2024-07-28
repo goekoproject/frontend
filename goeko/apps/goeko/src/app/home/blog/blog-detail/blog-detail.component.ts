ebe25ca26b30779e42633411b7a8d9513a79ed99
Belén Jiménez <b.jimenezsan5@gmail.com>
2024-07-28 21:00:52 +0200

    Data interpolation in blog detail and list and use of contenfulService for method and obtain entryId

--- goeko/apps/goeko/src/app/home/blog/blog-detail/blog-detail.component.ts
+++ goeko/apps/goeko/src/app/home/blog/blog-detail/blog-detail.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { ContentFulService, ContentFulModule } from '@goeko/store'
+import { map } from 'rxjs'
+import { GoDateFormatPipe } from '@goeko/ui'
+import { TranslateModule } from '@ngx-translate/core'
+import { RouterModule } from '@angular/router'
+import { switchMap } from 'rxjs'
+import { from } from 'rxjs'
+
 
 @Component({
   selector: 'goeko-blog-detail',
+  standalone: true,
+  imports: [CommonModule, RouterModule, TranslateModule, ContentFulModule, GoDateFormatPipe],
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss'],
 })
 export class BlogDetailComponent {
+  public blogPost: any;
   private get _blogId() {
     return this._route?.snapshot.params?.['blogId']
   }
-  constructor(private _route: ActivatedRoute) {
+
+  constructor(private _route: ActivatedRoute, private _contentFulService: ContentFulService) {
     console.log('post', this._blogId)
-    
+
+  }
+
+  ngOnInit(): void {
+    const blogId = this._blogId;
+    if (blogId) {
+      this._contentFulService.getEntryId(blogId).subscribe(post => {
+        this.blogPost = post;
+      });
+    }
   }
 }
+
