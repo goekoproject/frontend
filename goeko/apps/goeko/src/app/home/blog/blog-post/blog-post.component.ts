a4ba2fb52c7a5c503180a2f4abd930a3909b4447
Oswald Alessandro <cuman14@gmail.com>
2024-08-02 12:40:32 +0200

    chore: Update blog-post component to use 'summary' field instead of 'content' field

--- goeko/apps/goeko/src/app/home/blog/blog-post/blog-post.component.ts
+++ goeko/apps/goeko/src/app/home/blog/blog-post/blog-post.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common'
 import { Component, Input } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { AssetsContentFulPipe } from '@goeko/store'
-import { GoDateFormatPipe } from '@goeko/ui'
+import { GoDateFormatPipe, MdToHtmlPipe } from '@goeko/ui'
 import { TranslateModule } from '@ngx-translate/core'
 
 @Component({
   selector: 'goeko-blog-post',
   standalone: true,
-  imports: [CommonModule, RouterModule, GoDateFormatPipe, TranslateModule, AssetsContentFulPipe],
+  imports: [CommonModule, RouterModule, GoDateFormatPipe, TranslateModule, AssetsContentFulPipe, MdToHtmlPipe],
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss'],
 })
