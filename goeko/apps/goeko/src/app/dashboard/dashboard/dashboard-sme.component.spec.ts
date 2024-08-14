492a4e3b955397c4781909c3c30e0e9541874a44
Belén Jiménez <b.jimenezsan5@gmail.com>
2024-08-14 23:45:12 +0200

    Added 5 test with success but not finished

--- goeko/apps/goeko/src/app/dashboard/dashboard/dashboard-sme.component.spec.ts
+++ goeko/apps/goeko/src/app/dashboard/dashboard/dashboard-sme.component.spec.ts
@@ -3,7 +3,7 @@ import { signal } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ActivatedRoute, Router, RouterModule } from '@angular/router'
 import { MessageService } from '@goeko/business-ui'
-import { ProjectService, SmeUser, UserService } from '@goeko/store'
+import { ProjectService, SmeRequestResponse, SmeUser, UserService } from '@goeko/store'
 import { ButtonModule, DialogMessageModule, GoDateFormatPipe, GoInputModule } from '@goeko/ui'
 import { TranslateModule } from '@ngx-translate/core'
 import { of } from 'rxjs'
@@ -73,4 +73,36 @@ describe('DashboardSmeComponent', () => {
     expect(mockProjectService.getRecommendationsByProjectById).toHaveBeenCalled()
     expect(component.projects.length).toBeGreaterThan(0)
   })
+
+  // Prueba para probar el método ngOnInit, verificar que inicializa la propiedad projects como nueva instancia de array vacío
+  it('should initialize projects as an empty array on ngOnInit', () => {
+    component.ngOnInit();
+
+    expect(component.projects).toEqual([]);
+  });
+
+  //Prueba para verificar que 'projects' se inicializa correctamente con los datos del servicio.
+  it('should update projects with the first 3 recommendations from the service', () => {
+    const mockProject: SmeRequestResponse[] = [
+      { id: '1', date: '2023-01-01', classifications: [], notification: { onNewEcosolution: true } },
+      { id: '2', date: '2023-01-02', classifications: [], notification: { onNewEcosolution: false } },
+      { id: '3', date: '2023-01-03', classifications: [], notification: { onNewEcosolution: true } }
+    ];
+
+    jest.spyOn(mockProjectService, 'getRecommendationsByProjectById').mockReturnValue(of(mockProject));
+
+    component['_getLastProjectName']();
+    const [projects] = component.projects;
+    expect(projects).toEqual(mockProject);
+  });
+
+  // Prueba cuando no se devuelven proyectos: ESTA PRUEBA ES POSIBLE QUE SOBRE PORQUE NO AUMENTA VALORES AL ANÁLISIS
+  it('should set projects to an empty array if no recommendations are returned', () => {
+    jest.spyOn(mockProjectService, 'getRecommendationsByProjectById').mockReturnValue(of([]));
+
+    component['_getLastProjectName']();
+    const [projects] = component.projects;
+    expect(projects).toEqual([]);
+  });
+
 })
