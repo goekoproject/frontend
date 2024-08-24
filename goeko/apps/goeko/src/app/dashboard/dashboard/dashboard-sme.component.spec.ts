3999b53225f92146c110328527f7058cd96724b1
Oswald Alessandro <cuman14@gmail.com>
2024-08-24 13:50:15 +0200

    feat: Update DashboardSmeComponent to fix import statements and remove unnecessary code

--- goeko/apps/goeko/src/app/dashboard/dashboard/dashboard-sme.component.spec.ts
+++ goeko/apps/goeko/src/app/dashboard/dashboard/dashboard-sme.component.spec.ts
@@ -1,26 +1,33 @@
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { signal } from '@angular/core';
-import { ComponentFixture,TestBed } from '@angular/core/testing';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { MessageService } from '@goeko/business-ui';
-import { MESSAGE_TYPE, MessageType } from '@goeko/ui';
-import { ProjectService, SmeRequestResponse, SmeUser, UserService } from '@goeko/store';
-import { ButtonModule, DialogMessageModule, GoDateFormatPipe, GoInputModule } from '@goeko/ui';
-import { TranslateModule } from '@ngx-translate/core';
-import { from, of, Subject } from 'rxjs';
-import { DashboardSmeComponent } from './dashboard-sme.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { provideLocationMocks } from '@angular/common/testing'
+import { Component, signal } from '@angular/core'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ActivatedRoute, Router, RouterModule } from '@angular/router'
+import { MessageService } from '@goeko/business-ui'
+import { ProjectService, SmeRequestResponse, SmeUser, UserService } from '@goeko/store'
+import { ButtonModule, DialogMessageModule, GoDateFormatPipe, GoInputModule } from '@goeko/ui'
+import { TranslateModule } from '@ngx-translate/core'
+import { from, of } from 'rxjs'
+import { DashboardSmeComponent } from './dashboard-sme.component'
 
+@Component({
+  standalone: true,
+  template: '',
+})
+class TestProjectComponent {}
 describe('DashboardSmeComponent', () => {
   let component: DashboardSmeComponent
   let fixture: ComponentFixture<DashboardSmeComponent>
   // Mock Services
   let mockUserService: Partial<UserService>
   let mockProjectService: Partial<ProjectService>
-  let mockMessageService: MessageService
+  let mockMessageService:  {
+    deleteMessage: jest.Mock
+  }
   //let mockRouterService: Partial<Router>
   let mockActivatedRoute: Partial<ActivatedRoute>
 
+  let router: Router
   beforeEach(async () => {
     mockUserService = {
       userProfile: signal({ id: '1' } as SmeUser),
@@ -42,7 +49,7 @@ describe('DashboardSmeComponent', () => {
       deleteMessage: jest.fn().mockReturnValue({
         afterClosed: jest.fn().mockReturnValue(of(true)),
       }),
-    } as any;
+    }
 
     /*mockRouterService = {
       navigate: jest.fn(),
@@ -57,8 +64,7 @@ describe('DashboardSmeComponent', () => {
       snapshot: {
         params: {},
       },
-    } as any;
-
+    } as any
 
     await TestBed.configureTestingModule({
       declarations: [DashboardSmeComponent],
@@ -69,8 +75,13 @@ describe('DashboardSmeComponent', () => {
         GoInputModule,
         DialogMessageModule,
         GoDateFormatPipe,
-        //RouterModule.forRoot([]),
-        RouterTestingModule.withRoutes([])
+
+        RouterModule.forRoot([
+          {
+            path: '../sme-analysis/projects/project',
+            component: TestProjectComponent,
+          },
+        ]),
       ],
       providers: [
         { provide: UserService, useValue: mockUserService },
@@ -78,11 +89,13 @@ describe('DashboardSmeComponent', () => {
         { provide: MessageService, useValue: mockMessageService },
         //{ provide: Router, useValue: mockRouterService },
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        provideLocationMocks(),
       ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(DashboardSmeComponent)
     component = fixture.componentInstance
+    router = TestBed.inject(Router)
     fixture.detectChanges()
   })
 
@@ -93,9 +106,9 @@ describe('DashboardSmeComponent', () => {
 
   // Prueba para probar el método ngOnInit, verificar que inicializa la propiedad projects como nueva instancia de array vacío
   it('should initialize projects as an empty array on ngOnInit', () => {
-    component.ngOnInit();
-    expect(component.projects).toEqual([]);
-  });
+    component.ngOnInit()
+    expect(component.projects).toEqual([])
+  })
 
   // Prueba para obtener el proyecto
   it('should fetch projects on initialization', () => {
@@ -103,21 +116,19 @@ describe('DashboardSmeComponent', () => {
     expect(component.projects.length).toBeGreaterThan(0)
   })
 
-
-
   //Prueba para verificar que 'projects' se inicializa correctamente con los datos del servicio.
   it('should update projects with the first 3 recommendations from the service', () => {
     const mockProject: SmeRequestResponse[] = [
       { id: '1', date: '2023-01-01', classifications: [], notification: { onNewEcosolution: true } },
       { id: '2', date: '2023-01-02', classifications: [], notification: { onNewEcosolution: false } },
-      { id: '3', date: '2023-01-03', classifications: [], notification: { onNewEcosolution: true } }
-    ];
+      { id: '3', date: '2023-01-03', classifications: [], notification: { onNewEcosolution: true } },
+    ]
 
-    jest.spyOn(mockProjectService, 'getRecommendationsByProjectById').mockReturnValue(from(mockProject));
+    jest.spyOn(mockProjectService, 'getRecommendationsByProjectById').mockReturnValue(from(mockProject))
 
-    component['_getLastProjectName']();
-    expect(component.projects).toEqual(mockProject);
-  });
+    component['_getLastProjectName']()
+    expect(component.projects).toEqual(mockProject)
+  })
 
   // Prueba para goToProject:
   it('should navigate to the correct project when goToProject is called', () => {
@@ -126,19 +137,30 @@ describe('DashboardSmeComponent', () => {
       date: '2023-01-01',
       classifications: [],
       notification: { onNewEcosolution: true },
-    };
+    }
 
-    const navigateSpy = jest.spyOn(TestBed.inject(Router), 'navigate');
+    const navigateSpy = jest.spyOn(router, 'navigate')
 
-    component.goToProject(project);
+    component.goToProject(project)
 
-    expect(navigateSpy).toHaveBeenCalledWith(
-      ['../sme-analysis/projects/project', component.userProfile().id],
-      {
-        relativeTo: component.route.parent,
-        queryParams: { projectId: project.id },
-      }
-    );
-  });
+    expect(navigateSpy).toHaveBeenCalledWith(['../sme-analysis/projects/project', component.userProfile().id], {
+      relativeTo: component.route.parent,
+      queryParams: { projectId: project.id },
+    })
+  })
 
+  it('should delete the project when deleteProject is called', () => {
+    const project: SmeRequestResponse = {
+      id: '123',
+      date: '2023-01-01',
+      classifications: [],
+      notification: { onNewEcosolution: true },
+    }
+
+    component.deleteProject(project)
+
+    expect(mockMessageService.deleteMessage).toHaveBeenCalled()
+/*     expect(mockProjectService.deleteProject).toHaveBeenCalledWith(project.id)
+    expect(mockProjectService.getRecommendationsByProjectById).toHaveBeenCalled() */
+  })
 })
