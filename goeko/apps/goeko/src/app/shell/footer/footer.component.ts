95f628f8c64f3ab1a7a63e7dc96f4beafa00c8f9
Vortice87 <v.u.bedmar@gmail.com>
2024-09-03 20:36:46 +0200

    footer styles

--- goeko/apps/goeko/src/app/shell/footer/footer.component.ts
+++ goeko/apps/goeko/src/app/shell/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, ViewEncapsulation } from '@angular/core'
   encapsulation: ViewEncapsulation.None,
   // eslint-disable-next-line @angular-eslint/no-host-metadata-property
   host: {
-    class: 'footer-wrapper',
+    class: 'footer-wrapper mt-44',
   },
 })
 export class FooterComponent {
