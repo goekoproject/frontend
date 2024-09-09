82286439cf54b50c5636f5bb076649a81d9861b9
Oswald Alessandro <cuman14@gmail.com>
2024-09-09 19:51:58 +0200

    refactor: Update GoDateFormatPipe to use tempo.formkit for date formatting

--- goeko/libs/ui/src/lib/utils/pipes/go-data-format.pipe.ts
+++ goeko/libs/ui/src/lib/utils/pipes/go-data-format.pipe.ts
@@ -13,6 +13,15 @@ export class GoDateFormatPipe implements PipeTransform {
   constructor(private _translateService: TranslateService) {
     this._translateService.onLangChange.pipe().subscribe((current) => this.langSignal.set(current.lang))
   }
+  /**
+   * Transforms a date value into a formatted string based on the provided style tokens.
+   * Uses the library from https://tempo.formkit.com/#install for date formatting.
+   * Documentation can be found at the provided link.
+   *
+   * @param value - The date value to format.
+   * @param styleTokens - The format style tokens (default: 'DD/MM/YYYY').
+   * @returns The formatted date string.
+   */
   transform(value: string | Date | null | any, styleTokens: string = 'DD/MM/YYYY'): string {
     if (value) {
       console.log(styleTokens)
