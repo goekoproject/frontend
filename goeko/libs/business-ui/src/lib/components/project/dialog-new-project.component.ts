521f9c94b4c3655ddb349be16ad3e197e5719938
Belén Jiménez <b.jimenezsan5@gmail.com>
2024-09-20 12:54:21 +0200

    Creation new standalone component DialogNewProject and exported to public.api

---
+++ goeko/libs/business-ui/src/lib/components/project/dialog-new-project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'goeko-dialog-new-project',
+  templateUrl: './dialog-new-project.component.html',
+  styleUrls: ['./dialog-new-project.component.scss'],
+  standalone: true
+})
+export class DialogNewProjectComponent {
+  // Lógica del componente aquí
+}
