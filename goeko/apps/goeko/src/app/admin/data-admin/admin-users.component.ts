f9a95b728e8befa85c48724a27f1395982685bb1
Belén Jiménez <b.jimenezsan5@gmail.com>
2024-09-29 17:57:15 +0200

    Modified methods for show an error message in admin when cleantech with ecosolution

--- goeko/apps/goeko/src/app/admin/data-admin/admin-users.component.ts
+++ goeko/apps/goeko/src/app/admin/data-admin/admin-users.component.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { MessageService } from '@goeko/business-ui'
 import { CleanTechService, CleantechsUser, SmeService, SmeUser, USER_TYPE, UserService, UserType } from '@goeko/store'
-import { MESSAGE_TYPE } from '@goeko/ui'
+import { DialogConfig, DialogMessageService, MESSAGE_TYPE } from '@goeko/ui'
 import { Observable } from 'rxjs'
 import { DATA_ACTOR_SWITCH } from '../data-actors-switch.constants'
+import { TranslateService } from '@ngx-translate/core'
+import { EcosolutionsService } from '@goeko/store'
 
 interface User {
   id: number
@@ -74,29 +76,60 @@ export class AdminUserComponent {
     private _cleantechServices: CleanTechService,
     private MessageService: MessageService,
     private _userService: UserService,
+    private _translateService: TranslateService,
+    private _ecosolutionsService: EcosolutionsService,
+    private _dialogMessageService: DialogMessageService
   ) {}
 
   changeUserType(type: USER_TYPE): void {
     this.selectedUserType = type
     this.dataSources = this._dataSourcesByUserType[type as keyof DataSourcesByUserType] as Observable<SmeUser[] | CleantechsUser[]>
   }
+
   deleteUser(id: string): void {
+    if (this.selectedUserType === USER_TYPE.CLEANTECH) {
+      console.log('ID user Cleantech:', id);
+      this._ecosolutionsService.getEcosolutionsByCleantechId(id).subscribe(
+        (ecosolutions: any[]) => {
+          if (ecosolutions && ecosolutions.length > 0) {
+            this._dialogMessageService.open(this._getDataDialogError());
+          } else {
+            this._showDeleteWarning(id);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      this._showDeleteWarning(id);
+    }
+  }
+
+  private _getDataDialogError(): DialogConfig {
+    return {
+      body: this._translateService.instant('DIALOG.messageErrorDeleteCleantech'),
+      buttonPrimary: this._translateService.instant('accept'),
+    };
+  }
+
+  private _showDeleteWarning(id: string): void {
     this.MessageService.deleteMessage(MESSAGE_TYPE.WARNING, `${id}`)
       .afterClosed()
       .subscribe((isConfirmed) => {
         if (isConfirmed) {
           switch (this.selectedUserType) {
             case USER_TYPE.SME:
-              this._deleteSmeUser(id)
-              return
+              this._deleteSmeUser(id);
+              return;
             case USER_TYPE.CLEANTECH:
-              this._deleteCleantechUser(id)
-              return
+              this._deleteCleantechUser(id);
+              return;
             default:
-              console.error(`Unsupported user type: ${this.selectedUserType}`)
+              console.error(`Unsupported user type: ${this.selectedUserType}`);
           }
         }
-      })
+      });
   }
 
   private _deleteSmeUser(id: string): void {
@@ -107,7 +140,7 @@ export class AdminUserComponent {
 
   private _deleteCleantechUser(id: string): void {
     this._cleantechServices.deleteCleantechUser(id).subscribe(() => {
-      this.dataSources = this._cleantechServices.getAllCleantechData()
+      this.dataSources = this._cleantechServices.getAllCleantechData();
     })
   }
 }
