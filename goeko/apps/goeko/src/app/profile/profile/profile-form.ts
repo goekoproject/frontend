faac226774bb3d748ee38faf1d8394805f73e5d3
Oswald Alessandro <cuman14@gmail.com>
2024-10-02 16:11:08 +0200

    Refactor profile-form.ts to improve code readability and maintainability

--- goeko/apps/goeko/src/app/profile/profile/profile-form.ts
+++ goeko/apps/goeko/src/app/profile/profile/profile-form.ts
@@ -1,62 +1,43 @@
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  Validators,
-  ValidationErrors,
-  AbstractControl
-} from '@angular/forms';
-
-function validateCompanyId(): (control: AbstractControl) => ValidationErrors | null {
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms'
+const IdentifierRegexs = {
+  ES: /^[A-Z0-9]{9}$/,
+  FR: /^[0-9A-Z]{14}$/,
+  CH: /^[0-9]{6}$/,
+  DE: /^[0-9]{8,10}$/,
+  IT: /^[0-9]{11}$/,
+}
+export function validateCompanyId(): (control: AbstractControl) => ValidationErrors | null {
   return (control: AbstractControl): ValidationErrors | null => {
-    const country = control.parent?.get('companyCountry')?.value;
-    const id = control.value;
-    let regex;
-
-    switch (country) {
-      case 'ES':
-        regex = /^[A-Z0-9]{9}$/;
-        break;
-      case 'FR':
-        regex = /^[0-9A-Z]{14}$/;
-        break;
-      case 'CH':
-        regex = /^[0-9]{6}$/;
-        break;
-      case 'DE':
-        regex = /^[0-9]{8,10}$/;
-        break;
-      case 'IT':
-        regex = /^[0-9]{11}$/;
-        break;
-      default:
-        return null;
+    const country = control.parent?.get('companyCountry')?.value
+    if (!country) {
+      return null
     }
-    return regex && !regex.test(id) ? { invalidCompanyId: true } : null;
-  };
+    const id = control.value
+    const regex = IdentifierRegexs[country as keyof typeof IdentifierRegexs]
+
+    return regex && !regex.test(id) ? { invalidCompanyId: true } : null
+  }
 }
 
 export const smeFormGroup = new FormGroup({
-  name: new FormControl('',Validators.required),
-  email: new FormControl('',[Validators.required, Validators.email]),
+  name: new FormControl('', Validators.required),
+  email: new FormControl('', [Validators.required, Validators.email]),
   locations: new FormArray([]),
   website: new FormControl(),
-  employees: new FormControl('',[Validators.required, Validators.min(1)]),
+  employees: new FormControl('', [Validators.required, Validators.min(1)]),
   externalId: new FormControl(),
   companyCountry: new FormControl('', Validators.required),
-  companyId: new FormControl('', [Validators.required, validateCompanyId()])
-});
+  companyId: new FormControl('', [Validators.required, validateCompanyId()]),
+})
 
 export const cleanTechFormGroup = new FormGroup({
   name: new FormControl('', Validators.required),
-  email: new FormControl('', [Validators.email,Validators.required]),
-  country: new FormControl('',Validators.required),
+  email: new FormControl('', [Validators.email, Validators.required]),
+  country: new FormControl('', Validators.required),
   link: new FormControl(),
   logo: new FormControl(),
   city: new FormControl(),
   externalId: new FormControl(),
   companyCountry: new FormControl('', Validators.required),
-  companyId: new FormControl('', [Validators.required, validateCompanyId()])
-});
-
-
+  companyId: new FormControl('', [Validators.required, validateCompanyId()]),
+})
