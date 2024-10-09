ac92383fbb3db874ccba5b7dccc5a726077296b3
Oswald Alessandro <cuman14@gmail.com>
2024-10-09 12:20:51 +0200

    Refactor ecosolutions-search service and resolver

--- goeko/apps/goeko/src/app/projects/project-catalog-detail/project-catalog-detail.component.ts
+++ goeko/apps/goeko/src/app/projects/project-catalog-detail/project-catalog-detail.component.ts
@@ -1,18 +1,29 @@
 import { CommonModule } from '@angular/common'
-import { Component, input } from '@angular/core'
-import { SdgIconsComponent } from '@goeko/business-ui'
+import { Component, input, signal } from '@angular/core'
+import { CATEGORIES, PictureGetUrlPipe, SdgIconsComponent } from '@goeko/business-ui'
+import { SafePipe } from '@goeko/coretools'
 import { DataSelect, EcosolutionSearchResponse } from '@goeko/store'
-import { BadgeModule } from '@goeko/ui'
+import { BadgeModule, InputFileComponent, PercentageModule } from '@goeko/ui'
 import { TranslateModule } from '@ngx-translate/core'
 
 @Component({
   selector: 'goeko-project-catalog-detail',
   standalone: true,
-  imports: [CommonModule, TranslateModule, BadgeModule, SdgIconsComponent],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    BadgeModule,
+    SdgIconsComponent,
+    SafePipe,
+    InputFileComponent,
+    PictureGetUrlPipe,
+    PercentageModule,
+  ],
   templateUrl: './project-catalog-detail.component.html',
   styleUrl: './project-catalog-detail.component.scss',
 })
 export class ProjectCatalogDetailComponent {
   public dataSelect = DataSelect as any
+  public CATEGORIES = signal(CATEGORIES)
   ecosolutionSearchDetail = input.required<EcosolutionSearchResponse>()
 }
