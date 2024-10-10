3404fdd63b987423bdfe0170282f4cf9a5109ce5
Vortice87 <v.u.bedmar@gmail.com>
2024-10-10 13:51:02 +0200

    add form in request demo component

--- goeko/apps/goeko/src/app/home/request-demo-dialog/request-demo-dialog.component.ts
+++ goeko/apps/goeko/src/app/home/request-demo-dialog/request-demo-dialog.component.ts
@@ -1,19 +1,60 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, FormsModule, ReactiveFormsModule }  from '@angular/forms';
 import { DialogService } from '@goeko/ui'
 import { TranslateModule } from '@ngx-translate/core'
+import { Validators } from 'ngx-editor';
 
 @Component({
   selector: 'goeko-request-demo-dialog',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule,FormsModule,
+    ReactiveFormsModule],
   templateUrl: './request-demo-dialog.component.html',
   styleUrl: './request-demo-dialog.component.scss',
 })
-export class RequestDemoDialogComponent {
+export class RequestDemoDialogComponent implements OnInit{
+
   private _dialogService = inject(DialogService)
+  newSector: boolean= false;
+  constructor(
+    private _fb: FormBuilder,
+  ) {}
+
+  public formRequestDemo!: FormGroup;
+
+  ngOnInit(): void {
+    this.formRequestDemo = this._fb.group({
+      company: ['', [Validators.required]],
+      sector: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      otherSector: ['', [Validators.required]],
+    })
+
+    this.formRequestDemo.controls['sector'].valueChanges.subscribe(res => {
+        if(res === 'Other') {
+          this.newSector = true;
+        } else {
+          this.newSector = false;
+        }
+    })
+  }
+
 
   closeDialog(isAccepted: boolean = false) {
     this._dialogService.close(isAccepted)
   }
+
+  send(): void {
+    console.log(this.formRequestDemo.value);
+  }
+
+  // getSectorValue(): void {
+  //   if(this.formRequestDemo.controls['sector'].value === 'other'){
+  //     this.newSector = true;
+  //   } else {
+  //     this.newSector = false;
+  //   }
+  // }
 }
