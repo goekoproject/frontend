e330e75251f390c6b17578e15686ce4122da86d8
Oswald Alessandro <cuman14@gmail.com>
2024-10-22 15:37:03 +0200

    Refactor UI components for improved user experience and code readability

---
+++ goeko/libs/store/src/lib/classificactions/classification-categories-code.constant.ts
@@ -0,0 +1,6 @@
+export const CLASSIFICATION_CATEGORIES_CODE = {
+  CO2_EMISSION: 'co2Emission',
+  HAZARDOUS_PRODUCT: 'hazardousProduct',
+  WASTE: 'waste',
+  WATER: 'waterConsumption',
+}
--- goeko/libs/store/src/lib/classificactions/classifications.service.ts
+++ goeko/libs/store/src/lib/classificactions/classifications.service.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from '@angular/common/http'
-import { inject, Injectable, signal } from '@angular/core'
+import { computed, inject, Injectable, signal } from '@angular/core'
 import { toSignal } from '@angular/core/rxjs-interop'
-import { CODE_LANG } from '@goeko/core'
 import { TranslateService } from '@ngx-translate/core'
 import { forkJoin, map, Observable, shareReplay } from 'rxjs'
+import { CLASSIFICATION_CATEGORIES_CODE } from './classification-categories-code.constant'
 import { Category } from './classifications.interface'
 import { mergeCategoriesSectionWithClassificationCategory } from './transform.util'
 
+const sortCategories = (categories: Category[]): Category[] => {
+  const order = [
+    CLASSIFICATION_CATEGORIES_CODE.CO2_EMISSION,
+    CLASSIFICATION_CATEGORIES_CODE.WASTE,
+    CLASSIFICATION_CATEGORIES_CODE.WATER,
+    CLASSIFICATION_CATEGORIES_CODE.HAZARDOUS_PRODUCT,
+  ]
+  return categories.sort((a, b) => order.indexOf(a.code) - order.indexOf(b.code))
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -14,22 +23,19 @@ export class ClassificationsService {
   private _http = inject(HttpClient)
   private _translateService = inject(TranslateService)
   langSignal = signal(this._translateService.currentLang || this._translateService.defaultLang)
+  currentLang = computed(() => (this.langSignal() === 'en' ? 'gb' : this.langSignal()))
 
   private categories = toSignal(this.getClassificationsCategory(), { initialValue: [] })
 
-  groupingFormCategories(grouping: string = 'construction'): Observable<Category[]> {
-    return this._http
-      .get<Category[]>(`/v1/classifications/grouping/form/code/${grouping}/depth/translated?lang=${this.langSignal()}`)
-      .pipe(shareReplay(1))
+  groupingFormCategories(grouping = 'construction'): Observable<Category[]> {
+    return this._http.get<Category[]>(`/v1/classifications/grouping/form/code/${grouping}/depth/translated?lang=${this.langSignal()}`).pipe(
+      map((categories) => sortCategories(categories)),
+      shareReplay(1),
+    )
   }
 
   constructor() {
-    this._translateService.onLangChange
-      .pipe()
-      .subscribe((current) =>
-        this.langSignal.set(current.lang === CODE_LANG.EN ? CODE_LANG.GB : current.lang)
-
-    )
+    this._translateService.onLangChange.pipe().subscribe((current) => this.langSignal.set(current.lang))
   }
 
   getClassificationsCategory(): Observable<any[]> {
@@ -38,7 +44,7 @@ export class ClassificationsService {
       .pipe(map((classificationCategory) => mergeCategoriesSectionWithClassificationCategory(classificationCategory as any)))
   }
   getClassificationForCategoryTranslated(category: string): Observable<any> {
-    return this._http.get<any>(`/v1/classifications/category/translated/${category}?lang=${this.langSignal()}`)
+    return this._http.get<any>(`/v1/classifications/category/translated/${category}?lang=${this.currentLang()}`)
   }
 
   getAllDataCategories() {
