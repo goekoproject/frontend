653340d00eea4288760a45090135af934f2f0a0f
Vortice87 <v.u.bedmar@gmail.com>
2024-10-24 17:40:21 +0200

    load main photo from contentful

--- goeko/apps/goeko/src/app/home/front-main/front-main.component.ts
+++ goeko/apps/goeko/src/app/home/front-main/front-main.component.ts
@@ -1,15 +1,18 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation, effect } from '@angular/core'
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation, effect, inject } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
 import { HomeService } from '../home.service'
 import { DialogService } from '@goeko/ui'
 import { RequestDemoDialogComponent } from '../request-demo-dialog/request-demo-dialog.component'
 import { Router } from '@angular/router'
+import { ContentFulService } from '@goeko/store'
+import { map } from 'rxjs'
 
 enum ENTRYS_ID {
   MAIN = '6kOjxhcZv8tluQqyVzMglp',
   CONNECTING = '5K722xUKUczzRHt5COUgLp',
   SUSTAINABILITY = '2hwIsU3aiyyd2RhzFIgvw0',
 }
+const CONTENT_TYPE_MAIN_PHOTO = 'mainPhoto';
 
 @Component({
   selector: 'front-main',
@@ -19,6 +22,10 @@ enum ENTRYS_ID {
 
 })
 export class FrontMain implements OnInit {
+
+  private _contentFulService = inject(ContentFulService)
+  public mainPhoto$ = this._contentFulService.getContentType(CONTENT_TYPE_MAIN_PHOTO).pipe(map((items) => items.items));
+  public mainPhoto!: any;
 
   @ViewChild('goekoText') goekoText!: ElementRef;
 
@@ -41,6 +48,7 @@ export class FrontMain implements OnInit {
 
 
   ngOnInit(): void {
+    this._loadMainPhoto();
     this.currentLang = this._translate.defaultLang
     this._homeService.getSloganSustainability(ENTRYS_ID.SUSTAINABILITY)
     this._homeService.getSloganConnecting(ENTRYS_ID.CONNECTING)
@@ -52,13 +60,22 @@ export class FrontMain implements OnInit {
     effect(() => {
       this.entryDataConnecting = this._homeService.entryDataConnecting()
       this.entryDataMain = this._homeService.entryDataMain()
+      this.goekoText.nativeElement.addEventListener('animationstart', () => {
+        this.showMainSlogan = true;
+      });
       this.goekoText.nativeElement.addEventListener('animationend', () => {
         this.goekoText.nativeElement.style.visibility = 'hidden';
-        this.showMainSlogan = true;
       });
     })
   }
 
+  _loadMainPhoto() {
+    this.mainPhoto$.subscribe((items:any) => {
+
+      this.mainPhoto = items[0].fields.photo.fields.file.url
+      console.log(this.mainPhoto);
+    });
+  }
 
   private _onChangeLang() {
     this._translate.onLangChange.subscribe((res) => {
