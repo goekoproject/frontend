f1f5230f69fd9e2b3ca3cb3fb15b4ba05229483c
Oswald Alessandro <cuman14@gmail.com>
2024-11-05 16:46:44 +0100

    refactor: update slider component and styles; enhance carousel functionality and layout

--- goeko/apps/goeko/src/app/home/banner/slider/slider.component.ts
+++ goeko/apps/goeko/src/app/home/banner/slider/slider.component.ts
@@ -1,55 +1,49 @@
-import { Component, ElementRef, inject, OnInit, Renderer2, ViewChild} from '@angular/core'
-import { ContentFulService } from '@goeko/store';
-import { map } from 'rxjs';
+import { Component, effect, ElementRef, inject, Renderer2, ViewChild } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { ContentFulService } from '@goeko/store'
+import { map } from 'rxjs'
 
-
-const CONTENT_TYPE_CAROUSEL = 'carousel';
-
-
+const CONTENT_TYPE_CAROUSEL = 'carousel'
+interface CarrouselContentFul {
+  companies: any[]
+  timeSeconds: number
+  fields: { file: string; title: string }[]
+}
 @Component({
   selector: 'go-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent implements OnInit {
-
-
-
-  @ViewChild('sliderTrack', { static: true }) slider!: ElementRef;
+export class SliderComponent {
+  @ViewChild('sliderTrack', { static: true }) slider!: ElementRef
   private _contentFulService = inject(ContentFulService)
-  public carousel$ = this._contentFulService.getContentType(CONTENT_TYPE_CAROUSEL).pipe(map((items) => items.items));
-  carousel!: any;;
+  public carousel = toSignal(
+    this._contentFulService.getContentType(CONTENT_TYPE_CAROUSEL).pipe(
+      map((items) => ({
+        numberCompanies: (items.items[0].fields as unknown as CarrouselContentFul).companies.length,
+        timming: (items.items[0].fields as unknown as CarrouselContentFul).timeSeconds,
+        companies: (items.items[0].fields as unknown as CarrouselContentFul).companies.map((company) => ({
+          ...company.fields,
+          url: company.fields.file.url,
+        })),
+      })),
+    ),
+  )
 
-  constructor(private _renderer: Renderer2,
-  ) {}
-
-
-  ngOnInit(): void {
-    this.carousel$.subscribe((items:any) => {
-      this._loadCarousel(items);
-     });
+  constructor(private _renderer: Renderer2) {
+    effect(() => {
+      console.log(this.carousel())
+      if (this.carousel()) {
+        this._loadCarousel(this.carousel())
+      }
+    })
   }
 
   _loadCarousel(items: any) {
-    let mapCompanies:any = [];
-    items[0].fields.companies.forEach((element: any) => {
-      mapCompanies.push(element.fields.file.url);
-    });
-    this.carousel = {
-      numberCompanies: mapCompanies.length,
-      timming: items[0].fields.timeSeconds,
-      companies: mapCompanies
-    }
-    let bannerRepeat: number = this.carousel.numberCompanies * 2;
-    let widthCalc:number= 250 * bannerRepeat;
-    this._renderer.setStyle(this.slider?.nativeElement, 'width', `${widthCalc}px`);
-    document.documentElement.style.setProperty('--home-slider-baner-companies', this.carousel.numberCompanies.toString());
-    document.documentElement.style.setProperty('--home-slider-baner-time', this.carousel.timming + 's');
-    console.log(this.carousel);
-    console.log(bannerRepeat);
-    console.log(widthCalc);
-
-
+    const bannerRepeat = items.numberCompanies * 2
+    const widthCalc = 250 * bannerRepeat
+    this._renderer.setStyle(this.slider?.nativeElement, 'width', `${widthCalc}px`)
+    document.documentElement.style.setProperty('--home-slider-baner-companies', items.numberCompanies.toString())
+    document.documentElement.style.setProperty('--home-slider-baner-time', items.timming + 's')
   }
-
 }
