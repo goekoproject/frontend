d9ac16c2d52fb4781c17a8cd0bcd47bcf74db541
Vortice87 <v.u.bedmar@gmail.com>
2024-11-09 01:48:30 +0100

    refactor

--- goeko/apps/goeko/src/app/home/services/services.component.ts
+++ goeko/apps/goeko/src/app/home/services/services.component.ts
@@ -6,8 +6,8 @@ import { map } from 'rxjs';
 import { HeaderService } from '../header/header.services';
 import { Services } from './services.model';
 
-const CONTENT_TYPE_ENTERPRISE_SERVICES = 'services';
-const CONTENT_TYPE_CLEANTECH_SERVICES = 'cleanTechServices';
+const CONTENT_TYPE_ENTERPRISE = 'services';
+const CONTENT_TYPE_CLEANTECH = 'cleanTechServices';
 
 @Component({
 	selector: 'goeko-services',
@@ -21,8 +21,8 @@ export class ServicesComponent implements OnInit {
   public services$:any;
 	public title!: string;
 	public text!: string;
-  public enterpriseServices!: Services;
-  public cleanTechServices!: Services;
+  public enterprise!: Services;
+  public cleanTech!: Services;
   step = 'step1';
 
 	constructor(
@@ -33,8 +33,8 @@ export class ServicesComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-    this._loadContent(CONTENT_TYPE_ENTERPRISE_SERVICES);
-    this._loadContent(CONTENT_TYPE_CLEANTECH_SERVICES);
+    this._loadContent(CONTENT_TYPE_ENTERPRISE);
+    this._loadContent(CONTENT_TYPE_CLEANTECH);
     this._setHeaderTheme();
     this._onChangeLang()
 	}
@@ -43,19 +43,19 @@ export class ServicesComponent implements OnInit {
     this._headerService.isDarkTheme.next(true);
   }
 
-   _loadContent(contentTypeService: string) {
+  private _loadContent(contentTypeService: string) {
     this.services$ = this._contentFulService.getContentType(contentTypeService).pipe(map((items) => items.items));
     this.services$.subscribe((items:any) => {
-      contentTypeService === CONTENT_TYPE_ENTERPRISE_SERVICES ?
-      this.enterpriseServices = new Services(items[0]) : this.cleanTechServices = new Services(items[0])
+      contentTypeService === CONTENT_TYPE_ENTERPRISE ?
+      this.enterprise = new Services(items[0]) : this.cleanTech = new Services(items[0])
     });
   }
 
   private _onChangeLang() {
     this._translateServices.onLangChange.subscribe((res) => {
       this.currentLangCode.set(res.lang)
-      this._loadContent(CONTENT_TYPE_ENTERPRISE_SERVICES);
-      this._loadContent(CONTENT_TYPE_CLEANTECH_SERVICES);
+      this._loadContent(CONTENT_TYPE_ENTERPRISE);
+      this._loadContent(CONTENT_TYPE_CLEANTECH);
     })
   }
 
