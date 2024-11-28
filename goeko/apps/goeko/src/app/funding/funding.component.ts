a43988475fb854b8499fcdfaef04b269696a9998
Oswald Alessandro <cuman74@gmail.com>
2024-11-28 20:02:54 +0100

    feat: add FundingComponent and update routing structure

---
+++ goeko/apps/goeko/src/app/funding/funding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'goeko-funding',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './funding.component.html',
+  styleUrl: './funding.component.scss',
+})
+export class FundingComponent {}
