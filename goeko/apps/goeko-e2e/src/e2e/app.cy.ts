bb58ac2b5f00f33d38c08d5481cd3e51d2257993
Oswald Alessandro <cuman74@gmail.com>
2025-01-18 19:04:26 +0100

    fix(e2e): streamline login flow by accepting cookie policy and simplifying login command

--- goeko/apps/goeko-e2e/src/e2e/app.cy.ts
+++ goeko/apps/goeko-e2e/src/e2e/app.cy.ts
@@ -1,20 +1,13 @@
-import { getButtonCookie, getIsPlatform } from '../support/app.po'
+import { getIsPlatform, setAcceptCookiePolicy } from '../support/app.po'
 
 describe('Login flow', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200/login')
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000)
-    getButtonCookie().click({ force: true, multiple: true })
+    setAcceptCookiePolicy()
   })
 
-  it('should display welcome message', () => {
-    const email = Cypress.env('email')
-    const password = Cypress.env('password')
-
-    cy.login(email, password)
+  it('should access to platform', () => {
+    cy.login()
     getIsPlatform().should('exist')
-
-    // Function helper example, see `../support/app.po.ts` file
   })
 })
