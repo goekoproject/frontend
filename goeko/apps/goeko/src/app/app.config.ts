b4593b4f0e0f44ce09e9cea22d2dbd58c20410fd
Oswald Alessandro <cuman74@gmail.com>
2025-02-26 10:14:30 +0100

    feat: set default language for translation service based on session storage

--- goeko/apps/goeko/src/app/app.config.ts
+++ goeko/apps/goeko/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app'
 import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config'
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling, withViewTransitions } from '@angular/router'
 import { handlerHttpErrorInterceptor } from '@goeko/business-ui'
-import { AuthService, ConfigModule } from '@goeko/core'
+import { AuthService, CODE_LANG, ConfigModule } from '@goeko/core'
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { environment } from '../environments/environment'
@@ -38,6 +38,7 @@ export const appConfig: ApplicationConfig = {
       }),
     ),
     provideTranslateService({
+      defaultLanguage: sessionStorage.getItem('lang') ?? CODE_LANG.FR,
       loader: {
         provide: TranslateLoader,
         useFactory: httpLoaderFactory,
