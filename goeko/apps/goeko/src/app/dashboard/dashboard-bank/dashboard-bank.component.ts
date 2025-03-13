f87b99161a60b7ac0c57bc4f7498357abf43c33d
Oswald Alessandro <cuman74@gmail.com>
2025-03-13 21:12:21 +0100

    feat: update dashboard bank component layout and translations; add chip component for leads

--- goeko/apps/goeko/src/app/dashboard/dashboard-bank/dashboard-bank.component.ts
+++ goeko/apps/goeko/src/app/dashboard/dashboard-bank/dashboard-bank.component.ts
@@ -3,12 +3,13 @@ import { Component, computed, input } from '@angular/core'
 import { TableLeadsBankComponent } from '@goeko/business-ui/components/leads-bank/table-leads-bank.component'
 import { BankDashboardRecord } from '@goeko/store/bank/bank-dashboard.interface'
 import { LeadBankResponse } from '@goeko/store/lead/bank/lead-bank.-response.interface'
+import { ChipComponent } from '@goeko/ui/chip/chip.component'
 import { TranslatePipe } from '@ngx-translate/core'
 
 @Component({
   selector: 'goeko-dashboard-bank',
   standalone: true,
-  imports: [CommonModule, TableLeadsBankComponent, TranslatePipe],
+  imports: [CommonModule, TableLeadsBankComponent, TranslatePipe, ChipComponent],
   templateUrl: './dashboard-bank.component.html',
   styleUrl: './dashboard-bank.component.scss',
 })
--- goeko/libs/ui/src/lib/ui/chip/chip.component.ts
+++ goeko/libs/ui/src/lib/ui/chip/chip.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
   standalone: true,
   imports: [CommonModule],
   template: `
-    <span class="rounded-full bg-greenPastel px-3 py-1.5 text-sm font-semibold text-greenLime">
+    <span class="block w-max rounded-full bg-greenPastel px-3 py-1.5 text-sm font-semibold text-greenLime">
       <ng-content></ng-content>
     </span>
   `,
