fb4dfde70356cdabb113b2dbb440cb1f9ce5c76a
Oswald Alessandro <cuman74@gmail.com>
2025-05-28 23:49:48 +0200

    fix: check id

--- goeko/apps/goeko/src/app/shell/header-user/header-user.component.ts
+++ goeko/apps/goeko/src/app/shell/header-user/header-user.component.ts
@@ -1,18 +1,17 @@
 import { Component, computed, inject } from '@angular/core'
 import { SelectI18nComponent } from '@goeko/business-ui'
 import { UserService } from '@goeko/store'
-import { BadgeModule } from '@goeko/ui'
 
 @Component({
   selector: 'goeko-header-user',
   standalone: true,
-  imports: [SelectI18nComponent, BadgeModule],
+  imports: [SelectI18nComponent],
   templateUrl: './header-user.component.html',
   styleUrls: ['./header-user.component.scss'],
 })
 export class HeaderUserComponent {
   private _userService = inject(UserService)
   userProfile = computed(() =>
-    this._userService.userProfile().id ? this._userService.userProfile() : this._userService.auth0UserProfile(),
+    this._userService.userProfile()?.id ? this._userService.userProfile() : this._userService.auth0UserProfile(),
   )
 }
